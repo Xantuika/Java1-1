75e22afcb64a06e51d33c9afecd0cede6952d537
Xantuika <105634214+Xantuika@users.noreply.github.com>
2022-09-22 11:52:11 +0300

    Update CashbackHackService.java

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -1,9 +1,9 @@
-package ru.netology;
+package ru.netology.service;
 
 public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        return boundary - (amount % boundary);
     }
 }
